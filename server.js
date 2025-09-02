45d4d330def0aaa185b958b1b9a726419732a9e8
ayoubgeb <115789575+ayoubgeb@users.noreply.github.com>
2025-09-02 13:57:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-pinterest-tool/         <- root of repo
- ├─ server.js           <- backend Node.js code
- ├─ package.json        <- Node.js dependencies
- ├─ .env.example        <- optional login/API info
- └─ public/             <- folder for frontend
-      └─ index.html     <- dashboard UI
+<script type="text/plain" data-filename="server.js">
+...
+</script>
