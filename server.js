e964e8f4e2605b4940c34300f9bd0572140f6dea
ayoubgeb <115789575+ayoubgeb@users.noreply.github.com>
2025-09-02 09:53:03 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+pinterest-tool/         <- root of repo
+ ├─ server.js           <- backend Node.js code
+ ├─ package.json        <- Node.js dependencies
+ ├─ .env.example        <- optional login/API info
+ └─ public/             <- folder for frontend
+      └─ index.html     <- dashboard UI
